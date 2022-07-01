04d932deaad628d517510d78be07f8123df75e1a
whosonfirst <whosonfirst@localhost>
2022-07-01 10:20:49 -0700

    snapshot: start working on more sophisticated options handle for docker-bin/index.sh; untested

--- docker-bin/index.sh
+++ docker-bin/index.sh
@@ -6,9 +6,15 @@ PROPERTIES_ORG=whosonfirst
 PROPERTIES_REPO=whosonfirst-properties
 PROPERTIES_LOCAL=/usr/local/data/whosonfirst-properties
 
+PROPERTIES_ALTERNATES=""
+PROPERTIES_EXCLUSIONS=""
+
 GITHUB_TOKEN_URI=constant://?val=s33kret
 
 ITERATOR_SOURCE="whosonfirst-data:\/\/?prefix=whosonfirst-data-&exclude=whosonfirst-data-venue-"
+
+CHECKOUT_BRANCH=
+PUSH_BRANCH=main
 
 GIT=`which git`
 RUNTIMEVAR=`which runtimevar`
@@ -20,11 +26,20 @@ USAGE=""
 
 # Get CLI options
 
-while getopts "o:r:s:t:h" opt; do
+while getopts "a:c:e:o:r:s:t:u:h" opt; do
     case "$opt" in
         h) 
 	    USAGE=1
 	    ;;
+	a)
+	    PROPERTIES_ALTERNATES+=("$OPTARG")
+	    ;;
+	c)
+	    CHECKOUT_BRANCH=$OPTARG
+	    ;;
+	e)
+	    PROPERTIES_EXCLUSIONS+=("$OPTARG")
+	    ;;	
 	o)
 	    PROPERTIES_ORG=$OPTARG
 	    ;;
@@ -37,6 +52,9 @@ while getopts "o:r:s:t:h" opt; do
 	t)
 	    GITHUB_TOKEN_URI=$OPTARG
 	    ;;
+	u)
+	    UPDATE_BRANCH=$OPTARG
+	    ;;
 	:   )
 	    echo "Unrecognized flag"
 	    ;;
@@ -48,14 +66,19 @@ then
     echo "usage: ./index.sh -options"
     echo "options:"
     echo "-h Print this message."
+    echo "-c An optional branch to checkout when performing updates. If not empty then this value will be used to set the -u (update branch) flag. (Default is ${CHECKOUT_BRANCH})."
     echo "-o The GitHub organization for the properties repo. (Default is ${PROPERTIES_ORG}.)"
     echo "-r The name of the properties repo. (Default is ${PROPERTIES_REPO}.)"
     echo "-s A whosonfirst/go-whosonfirst-iterate-organization URI source to defines repositories to index. (Default is ${ITERATOR_SOURCE}.)"    
     echo "-t A gocloud.dev/runtimevar URI referencing the GitHub API access token to use for updating {PROPERTIES_REPO}. (Default is ${GITHUB_TOKEN_URI}.)"
+    echo "-u The branch name where updates should be pushed. (Default is ${UPDATE_BRANCH})."
     exit 0
 fi
 
-# Start 
+if [ "${CHECKOUT_BRANCH}" != "" ]
+then
+    $UPDATE_BRANCH=$CHECKOUT_BRANCH
+fi
 
 # First get an access token for writing changes
 
@@ -79,7 +102,11 @@ ${INDEX_PROPERTIES} -iterator-uri org:///tmp -properties ${PROPERTIES_LOCAL}/pro
 
 cd ${PROPERTIES_LOCAL}
 
-${GIT} checkout -b updates
+if [ "${CHECKOUT_BRANCH}" != "" ]
+then
+    ${GIT} checkout -b ${CHECKOUT_BRANCH}
+fi
+
 ${GIT} add properties
 ${GIT} commit -m "update properties" properties
-${GIT} push origin updates
+${GIT} push origin ${UPDATE_BRANCH}
